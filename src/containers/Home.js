fe83bb22c8a6d693f8599e735f7467bf70499b0a
Slavica-Marinkovic <slavka2807@gmail.com>
2021-11-22 23:44:13 +0100

    Details Item

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -9,6 +9,7 @@ import Empty from '../Components/Empty/Empty-invoices';
 
 import { useSelector } from 'react-redux';
 import DeleteItem from '../Components/DeleteItems/DeleteItem';
+import InvoiceDetails from '../Components/InvoiceDetails/InvoiceDetails'
 
 const Home = () => {
   const data = useSelector((state) => state.invoice.invoice);
@@ -92,6 +93,7 @@ const Home = () => {
           <Empty />
         )}
       </div>
+      <InvoiceDetails />
     </div>
   );
 };
